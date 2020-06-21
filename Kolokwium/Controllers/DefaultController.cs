5f14d322e6e80c2613e4749d14323b50302cab3b
Pirer12 <pirer23@gmail.com>
2020-06-21 16:34:32 +0200

    Dodaj pliki projektów.

---
+++ Kolokwium/Controllers/DefaultController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kolokwium.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kolokwium.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private readonly IDBService dBService;
+        public DefaultController(IDBService service)
+        {
+            dBService = service;
+        }
+
+        [HttpGet]
+        [Route("championship")]
+        public IActionResult GetTracks()
+        {
+            var res = 0;
+            return Ok(res);
+        }
+
+        [HttpPost]
+        [Route("teams/{teamId}/players")]
+
+        public IActionResult InsertPlayer()
+        {
+            //IActionResult res;
+            //try
+            //{
+            //    res = Ok(dBService.GetTrackById(id));
+            //}
+            //catch (Exception e)
+            //{
+            //   res = NotFound($"Not found track with id {id}");
+            //}
+            //return res;
+            return null;
+        }
+
+    }
+}
---
+++ Kolokwium/Services/SqlServerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium.Services
+{
+    public class SqlServerService : IDBService
+    {
+        //private readonly s17188Context _context;
+
+        //public SqlServerService(s17188Context context)
+        //{
+        //    _context = context;
+        //}
+    }
+}
